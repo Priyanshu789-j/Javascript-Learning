7a766128f7435588f0973403a3cd1b75e2017986
Priyanshu789-j <joshipriyanshu746@gmail.com>
2026-03-02 10:40:47 +0530

    added conditional statements practice

---
+++ CONDITIONALS STATEMENTS/index.js
@@ -0,0 +1,43 @@
+console.log("hello priyanshu ");
+
+let age = 5;
+let grace = 3;
+console.log(age + grace);
+console.log(age - grace);
+console.log(age / grace);
+console.log(age * grace);
+console.log(age ** grace);
+console.log(age % grace);
+
+
+if (age > 18){
+  console.log("you can drive")
+  
+}
+else{
+  console.log("you cannot drive")
+}
+
+if (age > 18){
+  console.log("you can drive")
+  
+}
+else if(age == 0){
+  console.log("are you kidding")
+}
+else{
+  console.log("you cannot drive")
+}
+
+a = 4;
+b = 6;
+let c = a > b ? (a-b) : (b-a);
+console.log(c)
+
+// translatation :
+// if (a>b){
+//   let c = a-b
+// }
+// else{
+//   c = b-a
+// }
