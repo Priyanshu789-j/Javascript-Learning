0c41ac64fd00576b36ecb545943b77f6b6ae2278
Priyanshu789-j <joshipriyanshu746@gmail.com>
2026-03-04 12:34:51 +0530

    added functions to the practice

---
+++ FUNCTIONS IN JS/index.js
@@ -0,0 +1,22 @@
+function nice(name) {
+  console.log("hey " + name + "you are good ")
+  console.log("hey " + name + "you are cool ")
+  console.log("hey " + name + "you are cute ")
+
+}
+nice("priyanshu ")
+
+function sum(a,b){
+  //console.log(a+b)
+  return a + b
+
+}
+result = sum(2,4)
+console.log("the sum of these numbers is : ",result)
+
+const func1 = (x)=>{      //Arrow function
+  console.log("i am ana arrow ",x)
+}
+func1(32);
+func1(67);
+func1(35);
