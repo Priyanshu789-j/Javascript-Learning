e1082a7c60f1f449426ffe19e397601ba3865a0e
Priyanshu789-j <joshipriyanshu746@gmail.com>
2026-04-12 13:25:47 +0530

    added events learning to my practice

---
+++ EVENTS and more in JS/script.js
@@ -0,0 +1,9 @@
+let button = document.getElementById("btn")
+
+button.addEventListener("click",()=>{
+  //alert("hatt bsdk ")
+  document.querySelector(".box").innerHTML = "<b>wapas aagya bsdk</b> chl nikAL" //it changes the .box content
+  
+})
+// more events i can google 
+
